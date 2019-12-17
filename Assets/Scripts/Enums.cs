ef3bc4afe2b642128656f5436384544efb47bf70
DESKTOP-MC405SG\PcMax <fadi.a.ashy@gmail.com>
2019-12-17 15:22:12 +0200

    Full Build

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace Enums
+{
+    //we can list all the controls that the game would contain.
+    public enum ControlKeys{
+
+        LeftKey,
+
+        RightKey,
+
+        Action
+    };
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Important so we can get the correct Controls
+    [SerializeField]
+    int PlayerID;
+
+    [SerializeField]
+    float Speed;
+
+    ControlsManager controlsManager;
+
+    void Start()
+    {
+        //get the ControlManager in the scene
+        controlsManager = FindObjectOfType<ControlsManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(controlsManager.GetKey(PlayerID, ControlKeys.LeftKey)))
+        {
+            this.gameObject.GetComponent<Transform>().Translate(Vector2.left * Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(controlsManager.GetKey(PlayerID, ControlKeys.RightKey)))
+        {
+            this.gameObject.GetComponent<Transform>().Translate(Vector2.right * Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(controlsManager.GetKey(PlayerID, ControlKeys.Action)))
+        {
+            Debug.Log( "" + PlayerID + " Action Fired");
+        }
+    }
+}
